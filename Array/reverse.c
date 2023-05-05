404c9b866bea03fb86631bb5aa2ce3e9f29f78df
diptopaull <132697350+diptopaull@users.noreply.github.com>
2023-05-05 23:14:27 +0600

    Code

---
+++ Array/reverse.c
@@ -0,0 +1,13 @@
+int minElement(int a[], int n)
+{
+    int min = a[0];
+
+    for (int i = 1; i < n; i++)
+    {
+        if (a[i] < min)
+        {
+            min = a[i];
+        }
+    }
+    return min;
+}
